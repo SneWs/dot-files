c4f77192f2eb8b0bea41493c3f9fa260437f4bfb
Marcus Grenängen <marcus@grenangen.se>
2023-11-27 08:38:26 +0100

    Add fan tweaking helpers

---
+++ bin/fan-gaming.sh
@@ -0,0 +1,5 @@
+liquidctl --match Corsair set pump speed 100
+liquidctl --match Corsair set fan1 speed 85
+liquidctl --match Corsair set fan2 speed 85
+liquidctl --match Corsair set fan4 speed 100    
+liquidctl --match Corsair set fan6 speed 100
---
+++ bin/fan-silent.sh
@@ -0,0 +1,5 @@
+liquidctl --match Corsair set pump speed 75
+liquidctl --match Corsair set fan1 speed 20
+liquidctl --match Corsair set fan2 speed 20
+liquidctl --match Corsair set fan4 speed 20    
+liquidctl --match Corsair set fan6 speed 20
